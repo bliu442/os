a06f99fe22994ba089a71453d06358fb28be1d3d
liuben <1922447127@qq.com>
2023-04-03 23:25:06 +0800

    bios中断检测物理内存,开启分页模式

--- boot/setup.asm
+++ boot/setup.asm
@@ -28,6 +28,26 @@ SECTION setup
 	mov byte [gs:0xC], 'P'
 	mov byte [gs:0xD], 0xA4
 
+;内存检测
+memory_check:
+	xor ebx, ebx ;首次调用ebx = 0
+	mov di, ARDS_BUFFER ;ARDS存放地址
+	mov word [ARDS_TIMES_BUFFER], 0 ;检测次数,每次返回一个ARDS
+
+.loop:
+	mov eax, 0xE820
+	mov ecx, 20
+	mov edx, 0x534D4150
+	int 0x15
+	
+	jc memory_check_error
+
+	add di, cx
+	inc word [ARDS_TIMES_BUFFER]
+
+	cmp ebx, 0 ;bios写数据判断是否结束内存检测
+	jne .loop
+
 	;进入保护模式 构建GDT表,打开A20总线,cr0 PE位置一
 	xchg bx, bx
 
@@ -63,8 +83,87 @@ protected_mode:
 
 	mov byte [gs:160], 'p'
 
+	;开启分页 构建页目录表gdt和页表gtt 页目录表地址写入cr3 cr0的pg位置1
+	call setup_page
+
+	mov eax, PAGE_DIR_TABLE_POS
+	mov cr3, eax
+
+	mov eax, cr0
+	or eax, 0x80000000
+	mov cr0, eax
+
+	sgdt [gdt_ptr]
+
+	;开启分页后 操作的都是虚拟地址 访问gdt,显存段,esp都使用0xC0000000以上的地址
+	mov ebx, [gdt_ptr + 2]
+	or dword [ebx + 8 * 3 + 4], 0xC0000000 ;baseaddr 24-31
+
+	add dword [gdt_ptr + 2], 0xC0000000
+
+	add esp, 0xC0000000
+
+	lgdt [gdt_ptr]
+
+	mov byte [gs:160], 'v'
+
 	jmp $
 
+
+memory_check_error:
+	mov byte [gs:160], 'm'
+	mov byte [gs:162], 'e'
+	mov byte [gs:164], 'r'
+	mov byte [gs:168], 'r'
+
+	jmp $
+
+setup_page:
+;清零
+	mov ecx, 0x400
+	mov esi, 0
+.clear_pdt:
+	mov dword [PAGE_DIR_TABLE_POS + esi * 4], 0
+	inc esi
+	loop .clear_pdt
+
+.create_ptt:
+	mov eax, PAGE_DIR_TABLE_POS
+	add eax, 0x1000
+
+	or eax, PAGE_US_U | PAGE_RW_W | PAGE_P
+	mov [PAGE_DIR_TABLE_POS + 0x0], eax ;0 - 0x40000
+	mov [PAGE_DIR_TABLE_POS + 0xC00], eax ;映射到0xC0000000以上,仿linux操作系统内存空间 0xC0000000 - 0xC0040000
+	
+	sub eax, 0x1000
+	mov [PAGE_DIR_TABLE_POS + 0xFFC], eax ;最后一个页表存放页目录表本身 用于虚拟内存访问页目录表,页表
+
+;挂载0 - 1M物理内存
+	mov ebx, PAGE_DIR_TABLE_POS
+	add ebx, 0x1000
+	mov ecx, 0x100
+	mov esi, 0
+	mov edx, PAGE_US_U | PAGE_RW_W | PAGE_P
+.create_pte:
+	mov [ebx + esi * 4], edx
+	add edx, 0x1000
+	inc esi
+	loop .create_pte
+
+;先创建0xC0040000 - 0xFFFFFFFF的页表
+	mov eax, PAGE_DIR_TABLE_POS
+	add eax, 0x2000
+	or eax, PAGE_US_U | PAGE_RW_W | PAGE_P
+	mov ebx, PAGE_DIR_TABLE_POS
+	mov ecx, 0xFE
+	mov esi, 0xC00 >> 2 + 1
+.create_kernel_ptt:
+	mov [ebx + esi * 4], eax
+	inc esi
+	add eax, 0x1000
+	loop .create_kernel_ptt
+
+	ret
 
 SECTION gdt
 gdt_base:
